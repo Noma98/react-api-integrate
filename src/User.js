629becddce4015f1d5086cf434f618f252d48b78
Noma <wiostz98kr@gmail.com>
2021-09-28 22:09:05 +0900

    Use Context API

--- src/User.js
+++ src/User.js
@@ -1,24 +1,17 @@
-import React from 'react'
-import axios from 'axios';
-import { useAsync } from 'react-async';
+import React, { useEffect } from 'react';
+import { getUser, useUsersDispatch, useUsersState } from './UsersContext';
 
-//프로미스를 반환하는 함수의 파라미터를 객체 형태로 해주어야 함
-async function getUser({ id }) {
-    const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${id}`
-    );
-    return response.data;
-}
 
 function User({ id }) {
-    //렌더링하는 시점이 아닌 사용자의 특정 인터랙션에 따라 API를 호출하고 싶을 땐 promiseFn 대신 deferFn을 사용하고, reload 대신 run 함수를 사용하면 된다.
-    const { data: user, error, isLoading } = useAsync({
-        promiseFn: getUser,
-        id,
-        watch: id //watch에 설정한 값이 바뀔 때마다 promiseFn에 넣은 함수 재호출
-    })
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-    if (isLoading) return <div>로딩중</div>;
+    useEffect(() => {
+        getUser(dispatch, id);
+    }, [dispatch, id]);
+
+    const { data: user, loading, error } = state.user;
+    if (loading) return <div>로딩중</div>;
     if (error) return <div>에러 발생</div>;
     if (!user) return null;
 
