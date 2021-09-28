e284a1177bbcd0750b5f891ff1ae1e74740c0419
Noma <wiostz98kr@gmail.com>
2021-09-28 15:07:50 +0900

    Make useAsync hook

--- src/Users.js
+++ src/Users.js
@@ -1,68 +1,40 @@
-import React, { useEffect, useCallback, useReducer } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
+import User from './User';
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null
-            };
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.err,
-            };
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.users,
-                error: null
-            };
-        default:
-            throw new Error(`Unhandled action type: ${action.type}`);
-    }
+async function getUsers() {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    return response.data;
 }
-const initialState = {
-    loading: false,
-    data: null,
-    error: null,
-}
+
 function Users() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const { data: users, loading, error } = state;
+    const [state, refetch] = useAsync(getUsers, [], true);
+    const [userId, setUserId] = useState(null);
 
-    const fetchUsers = useCallback(async () => {
-        dispatch({ type: 'LOADING' });
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users'
-            );
-            dispatch({ type: 'SUCCESS', users: response.data });
-        } catch (err) {
-            dispatch({ type: 'ERROR', error: err });
-        }
-    }, []);
+    const { loading, data: users, error } = state;
 
-    useEffect(() => {
-        fetchUsers();
-    }, [fetchUsers]);
-
-    if (loading) return <div>로딩중</div>
-    if (error) return <div>에러 발생</div>
-    if (!users) return null;
+    if (loading) return <div>로딩중</div>;
+    if (error) return <div>에러 발생</div>;
+    if (!users) return <button onClick={refetch}>불러오기</button>;
     return (
-        <ul>
-            {users.map(user =>
-                <li key={user.id}>
-                    {user.username}({user.name})
-                </li>
-            )}
-            <button onClick={fetchUsers}>다시 불러오기</button>
-        </ul>
-    )
+        <>
+            <ul>
+                {users.map(user => (
+                    <li
+                        key={user.id}
+                        onClick={() => setUserId(user.id)}
+                    >
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button onClick={refetch}>다시 불러오기</button>
+            {userId && <User id={userId} />}
+        </>
+    );
 }
 
-export default Users
+export default Users;
