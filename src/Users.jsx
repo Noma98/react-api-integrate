385640b1e7db6104c80e9a4498a93e3c01e3863b
Noma <wiostz98kr@gmail.com>
2021-10-10 20:15:38 +0900

    Use react-async

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 
 
@@ -11,23 +11,24 @@ async function getUsers() {
     return response.data;
 }
 function Users() {
-    const [state, refetch] = useAsync(getUsers, [], true);
-    const { data: users, loading, error } = state;
+    const { data: users, error, isLoading, run } = useAsync({ deferFn: getUsers });
     const [userId, setUserId] = useState(null);
 
-    if (loading) return <div>로딩중</div>
+    if (isLoading) return <div>로딩중</div>
     if (error) return <div>에러 발생</div>
-    if (!users) return <button onClick={refetch}>불러오기</button>;
+    if (!users) return <button onClick={run}>불러오기</button>;
     return (
-        <ul>
-            {users.map(user =>
-                <li key={user.id} onClick={() => setUserId(user.id)}>
-                    {user.username}({user.name})
-                </li>
-            )}
-            <button onClick={refetch}>다시 불러오기</button>
-            {userId && <User id={userId} />}
-        </ul>
+        <>
+            <ul>
+                {users.map(user =>
+                    <li key={user.id} onClick={() => setUserId(user.id)}>
+                        {user.username}({user.name})
+                    </li>
+                )}
+                {userId && <User id={userId} />}
+            </ul>
+            <button onClick={run}>다시 불러오기</button>
+        </>
     )
 }
 
